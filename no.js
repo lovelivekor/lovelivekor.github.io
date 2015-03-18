8ecaad2e2646221e5f3e17c4ba55e9c8cea69caf
YJSoft <YJSoft@users.noreply.github.com>
2015-03-18 10:48:18 +0900

    Create no.js

---
+++ no.js
@@ -0,0 +1,7 @@
+var act2 = "<div class='script'>";
+    act2 = act2 + "<div class='black ko'>본 브라우저로는 이 서비스를 이용하실 수 없습니다.</div>";
+    act2 = act2 + "<div class='members1 ko'>This browser cannot use this service.</div>";
+    act2 = act2 + actor_all_3;
+  act2 = act2 + "</div>";
+  
+  $("#duration").html(act2);
