025e825198754019825fec6ac3d2f013666772e9
YJSoft <YJSoft@users.noreply.github.com>
2015-03-25 01:48:16 +0900

    Update no.js

--- no.js
+++ no.js
@@ -1,6 +1,10 @@
 var act2 = "<div class='script'>";
-    act2 = act2 + "<div class='black ko'>본 브라우저로는 이 서비스를 이용하실 수 없습니다.</div>";
-    act2 = act2 + "<div class='members1 ko'>This browser cannot use this service.</div>";
-  act2 = act2 + "</div>";
-  
-  $("#duration").html(act2);
+act2 = act2 + "<div class='black ko'>본 브라우저로는 이 서비스를 이용하실 수 없습니다.</div>";
+act2 = act2 + "<div class='members1 ko'>Your browser is not supported by this service.</div>";
+act2 = act2 + "</div>";
+
+$("#duration").html(act2);
+/* hide all control */
+$("#btns").html('');
+$("#mbtns").html('');
+$("#nowtime").html('');
